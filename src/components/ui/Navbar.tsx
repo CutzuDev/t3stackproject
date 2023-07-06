592e8e1dece05eda8bfc79672ab7be4a85fe8585
CutzuDev <alexfarkas05@gmail.com>
2023-07-06 21:26:03 +0300

    idk

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,38 @@
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+function Navbar() {
+  return (
+    <nav className="fixed flex h-20 w-full items-center justify-center border-b border-slate-200 border-opacity-30 bg-slate-200 bg-opacity-10">
+      <div className="flex w-full max-w-[1700px] items-center justify-between px-5">
+        <span className="select-none text-4xl hue-rotate-[190deg]">😼</span>
+        <div>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="rounded-md border border-slate-200 border-opacity-30 bg-white/10 px-4 py-2">
+                Sign In
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <div className="flex items-center justify-center gap-5">
+              <UserButton />
+              <SignOutButton>
+                <button className="rounded-md border border-slate-200 border-opacity-30 bg-white/10 px-4 py-2">
+                  Sign Out
+                </button>
+              </SignOutButton>
+            </div>
+          </SignedIn>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import { type AppType } from "next/app";
+import { dark } from "@clerk/themes";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "~/components/ui/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider
+      appearance={{
+        variables: {
+          borderRadius: "0.25rem",
+          colorPrimary: "#64748b",
+        },
+        baseTheme: dark,
+      }}
+      {...pageProps}
+    >
+      <Navbar />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
