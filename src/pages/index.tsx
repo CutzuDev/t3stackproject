47ec1c396ee79488726601ba5dcfd59e8fbed01e
CutzuDev <alexfarkas05@gmail.com>
2023-07-07 21:44:35 +0300

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { SignedIn, useUser } from "@clerk/nextjs";
 import Head from "next/head";
+import Image from "next/image";
 import { useState } from "react";
 import Divider from "~/components/utils/Divider";
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
 
 export default function Home() {
   const { data, isLoading } = api.posts.getAll.useQuery();
@@ -11,6 +12,8 @@ export default function Home() {
     const { user } = useUser();
 
     if (!user) return null;
+
+    console.log(user.username, user.id);
 
     const [searhVal, setsearhVal] = useState("");
 
@@ -24,6 +27,23 @@ export default function Home() {
           onChange={(event) => setsearhVal(event.target.value)}
         />
         <span>{searhVal.length}/250</span>
+      </div>
+    );
+  };
+
+  type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+  const PostView = (props: PostWithUser) => {
+    const { post, author } = props;
+    return (
+      <div className="flex w-full items-stretch justify-start gap-2.5 p-2.5">
+        <div className="relative h-10 w-10 overflow-hidden rounded-full">
+          <Image src={author.profilePicture} fill alt="" />
+        </div>
+        <div className="w-[2px] rounded-3xl bg-slate-200 bg-opacity-30"></div>
+        <div className="flex flex-col items-start justify-start">
+          <span className="text-sm opacity-50">@{author.username}</span>
+          <span className="">{post.content}</span>
+        </div>
       </div>
     );
   };
@@ -45,24 +65,23 @@ export default function Home() {
             <SignedIn>
               <CreatePostWizard />
             </SignedIn>
-            <div className="flex w-full max-w-[500px] flex-col items-center justify-center gap-1 rounded-md border border-slate-200 border-opacity-30 bg-slate-200 bg-opacity-10 ">
-              {!isLoading ? (
-                data ? (
-                  data.map((post, index) => (
-                    <div
-                      className="w-full items-center justify-start  p-2"
-                      key={index}
-                    >
-                      {post.content}
-                    </div>
-                  ))
+            {data?.length !== 0 && (
+              <div className="flex min-h-[44px] w-full max-w-[500px] flex-col items-center justify-center gap-1 rounded-md border border-slate-200 border-opacity-30 bg-slate-200 bg-opacity-10 ">
+                {!isLoading ? (
+                  data ? (
+                    data.map((fullPost, index) => (
+                      <PostView {...fullPost} key={index} />
+                    ))
+                  ) : (
+                    <div>Something went wrong</div>
+                  )
                 ) : (
-                  <div>Something went wrong</div>
-                )
-              ) : (
-                <div>Loading..</div>
-              )}
-            </div>
+                  <div>Loading..</div>
+                )}
+              </div>
+            )}
+
+            <div className="flex items-center justify-center gap-10"></div>
           </div>
         </section>
       </div>
