e1035feca79ac573c684c0e62b0e2cbe1fa07aeb
CutzuDev <alexfarkas05@gmail.com>
2023-07-11 11:00:56 +0300

    Push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,30 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "~/components/ui/Navbar";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider
-      appearance={{
-        variables: {
-          borderRadius: "0.25rem",
-          colorPrimary: "#64748b",
-        },
-        baseTheme: dark,
-      }}
-      {...pageProps}
-    >
-      <Navbar />
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <>
+      <Head>
+        <title>CutzuMojies</title>
+        <meta name="description" content="The best place to post emojies on!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ClerkProvider
+        appearance={{
+          variables: {
+            borderRadius: "0.25rem",
+            colorPrimary: "#64748b",
+          },
+          baseTheme: dark,
+        }}
+        {...pageProps}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </>
   );
 };
 
